b07ab055dc8c800d5e68101e5b7dd8f828ff45b3
thesayf <r_ori@hotmail.co.uk>
2015-12-25 16:43:22 +0000

    commit

--- amaretti-v1.1.4/html/app.js
+++ amaretti-v1.1.4/html/app.js
@@ -47,4 +47,52 @@ $routeProvider
     
 })
 
+.when('/addaddresses', {
+    templateUrl: 'pages/addaddresses.html',
+    controller: 'appController'
+    
+})
+
+.when('/inventorybook', {
+    templateUrl: 'pages/inventorybook.html',
+    controller: 'appController'
+    
+})
+
+.when('/profilepage', {
+    templateUrl: 'pages/profilepage.html',
+    controller: 'appController'
+    
+})
+
+.when('/savedquotespage', {
+    templateUrl: 'pages/savedquotespage.html',
+    controller: 'appController'
+    
+})
+
+.when('/upcomingjobspage', {
+    templateUrl: 'pages/upcomingjobspage.html',
+    controller: 'appController'
+    
+})
+
+.when('/completedjobspage', {
+    templateUrl: 'pages/completedjobspage.html',
+    controller: 'appController'
+    
+})
+
+.when('/viewallmessages', {
+    templateUrl: 'pages/viewallmessages.html',
+    controller: 'appController'
+    
+})
+
+.when('/paymentdetails', {
+    templateUrl: 'pages/paymentdetails.html',
+    controller: 'appController'
+    
+})
+
 });
